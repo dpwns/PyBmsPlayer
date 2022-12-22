ab53148fa0edde549e260a64192e893ef1f7f756
dpwns <dpwns0827@naver.com>
2022-12-23 02:07:14 +0900

    작업중

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # PyBmsPlayer
-
+## 개요
 Python 기반 Be-Music-Script 구동기입니다
 Bundle에 bms 파일을 넣으면 됩니다
-.bms/.bme/.bml 
+### 지원 파일
+.bms / .bme / .bml
---
+++ display.py
@@ -0,0 +1,11 @@
+import pygame
+
+Frame = 144
+Height = 720
+Width = 1280
+Text_size1 = 20
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
